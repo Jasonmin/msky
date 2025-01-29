a0d1e6b3d7e01101642754d5d7e568af6ec566bf
qz <qiangzhou3@iflytek.com>
2025-01-29 22:15:10 +0800

    c

---
+++ zzcommit.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+## 权限授予 chmod u+x *.sh
+# chmod u+x ./zzcommit.sh
+
+# git push
+
+# 获取提交信息
+message="`date +%y-%m-%d,%H:%M`"
+if [ "$1" != "" ] #前后保留空格
+then
+    message="$1"
+fi
+
+echo ======github page commit
+
+# bitbucket
+git remote set-url origin https://github.com/Jasonmin/msky.git
+git pull
+
+# 代码git提交
+git add .
+git commit -m 'c'
+git push
+
+
+# # 日志
+# # echo $message
+
+# # 提交并推送
+# git add .
+# git commit -m "$message"
+
+# # 拉取
+# # git pull
+# git push
